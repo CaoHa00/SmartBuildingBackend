13d94421db661222d589f0b3d2b9fcc1b392fb03
Hà Chí Cao <92386482+Hachicao@users.noreply.github.com>
2025-03-07 10:25:39 +0700

    create structure for project

---
+++ src/main/java/com/example/SmartBuildingBackend/SmartBuildingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartBuildingBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBuildingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBuildingBackendApplication.class, args);
+	}
+
+}
