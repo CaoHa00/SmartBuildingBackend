6036bb8eff1d8aafdd0629e68cc428ebb74060aa
Doanh Nguyen <xuandoanh122@gmail.com>
2025-03-11 11:05:20 +0700

    3-create-CRUD-floor

---
+++ src/main/java/com/example/SmartBuildingBackend/service/FloorService.java
@@ -0,0 +1,13 @@
+package com.example.SmartBuildingBackend.service;
+
+import java.util.List;
+
+import com.example.SmartBuildingBackend.dto.FloorDto;
+
+public interface FloorService {
+    List<FloorDto> getAllFloors();
+    FloorDto getFloorById(int floorId);
+    FloorDto updateFloor(int floorId, FloorDto updateFloor);
+    void deleteFloor(int floorId);
+    FloorDto addFloor(int blockId,FloorDto floorDto);
+}
