11a479aee73a777940436ae5123e65eb204fa91f
xuandoanh122 <xuandoanh122@gmail.com>
2025-03-17 11:45:10 +0700

    test database

--- src/main/java/com/example/SmartBuildingBackend/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/SmartBuildingBackend/Exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.example.SmartBuildingBackend.exception;
+package com.example.SmartBuildingBackend.Exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
